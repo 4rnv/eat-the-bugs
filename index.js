2726d8a3e3ebb49b0064432eb7491b4c3779109f
4rnv <arnavsdandekar@gmail.com>
2024-09-18 21:25:51 +0530

    fsdfsd

---
+++ index.js
@@ -0,0 +1,23 @@
+window.onload(
+    getResponse(),
+    sendData()
+);
+
+async function getResponse() {
+	const response = await fetch(
+		'http://ip-api.com/json/?fields=status,message,country,regionName,city,lat,lon,query',
+		{
+			method: 'GET',
+		}
+	);
+    const data = await response.json();
+	//console.log(data);
+    sendData(data);
+}
+
+function sendData(data) {
+    var ip = document.getElementById('IP');
+    var info = document.getElementById('info');
+    ip.innerHTML = `${data.query}`;
+    info.innerHTML = `<p>How's the weather in ${data.lat}${data.lon},${data.city},${data.regionName},${data.country}?<p>`;
+}
