53794c86737587e680784c8a1198b2ed160a1a82
4rnv <arnavsdandekar@gmail.com>
2024-09-19 21:40:36 +0530

    YQUVILLEVTZEBVGS

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function sendData(data) {
     var ip = document.getElementById('IP');
     var info = document.getElementById('info');
     ip.innerHTML = `${data.query}`;
-    info.innerHTML = `<p>How's the weather in ${data.lat}${data.lon},${data.city},${data.regionName},${data.country}?<p>`;
+    info.innerHTML = `<p>How is ze weather in ${data.lat} ${data.lon}<br>${data.city},${data.regionName},${data.country}?<p>`;
 }
